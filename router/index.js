d6257b03ed6a58073f79f9edd11961f77add9c22
ben071 <benjaminhiggs@icloud.com>
2019-08-17 22:19:39 +0100

    Add all avatars

--- router/index.js
+++ router/index.js
@@ -10,6 +10,16 @@ router.get("/", async (req, res) => {
         jackAvatar,
         codfishAvatar,
         foxeldruAvatar,
+        jaspAvatar,
+        samoyedAvatar,
+        pjayAvatar,
+        sentryAvatar,
+        tigerAvatar,
+        blueAvatar,
+        gideonAvatar,
+        jaydenAvatar,
+        kelwingAvatar,
+        vladAvatar,
     ] = await Promise.all([
         // IDs are in order like above
         "265569046425108481",
@@ -18,12 +28,21 @@ router.get("/", async (req, res) => {
         "291607550825332736",
         "102341036403068928",
         "302787989384462336",
-    ].map(id => users.fetch(id)
-         .then(user => user.displayAvatarURL())
+        "422650822254526474",
+        "195406632224555009",
+        "400840326836781067",
+        "552245318146850825",
+        "267103945346908161",
+        "511447531402559488",
+        "524371727812263948",
+        "503105733701926922",
+        "109710323094683648",
+        "139836912335716352",
+    ].map(id => users.fetch(id, true)
+         .then(user => `${user.displayAvatarURL()}?size=2048`)
          .catch(() => "https://cdn.discordapp.com/embed/avatars/0.png")
     ));
     res.render("index.ejs", {
-        client: req.client.server.client.user,
         avatars: {
             benAvatar,
             austinAvatar,
@@ -31,8 +50,18 @@ router.get("/", async (req, res) => {
             jackAvatar,
             codfishAvatar,
             foxeldruAvatar,
-        },
+            jaspAvatar,
+            samoyedAvatar,
+            pjayAvatar,
+            sentryAvatar,
+            tigerAvatar,
+            blueAvatar,
+            gideonAvatar,
+            jaydenAvatar,
+            kelwingAvatar,
+            vladAvatar,
+        }
     });
-})
+});
 
 module.exports = router;
