a43def24c745c347267ea1758ad2435676a7dd53
Vlad Frangu <kingdgrizzle@gmail.com>
2019-08-17 16:30:03 +0300

    src: Do the avatars properly

--- router/index.js
+++ router/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/", function(req, res) {
+router.get("/", async (req, res) => {
+    const { users } = req.client;
+    const [
+        benAvatar,
+        austinAvatar,
+        toastAvatar,
+        jackAvatar,
+        codfishAvatar,
+        foxeldruAvatar,
+    ] = await Promise.all([
+        // IDs are in order like above
+        "265569046425108481",
+        "219117197178568708",
+        "396459305961914369",
+        "291607550825332736",
+        "102341036403068928",
+        "302787989384462336",
+    ].map(id => users.fetch(id)))
     res.render("index.ejs", {
-        client: req.client.server.client.user
+        client: req.client.server.client.user,
+        avatars: {
+            benAvatar,
+            austinAvatar,
+            toastAvatar,
+            jackAvatar,
+            codfishAvatar,
+            foxeldruAvatar,
+        },
     });
 })
 
