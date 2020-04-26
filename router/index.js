bbbe3e88fea10a0e7598ba12df996abe1c68a14a
Ben071 <benjaminhiggs@icloud.com>
2020-04-26 01:12:46 +0100

    Update staff team

--- router/index.js
+++ router/index.js
@@ -8,39 +8,41 @@ router.get("/", async (req, res) => {
 
     const [
         benAvatar,
-        austinAvatar,
         toastAvatar,
         jackAvatar,
         kelwingAvatar,
         lewisAvatar,
+        austinAvatar,
         jaspAvatar,
+        karnageAvatar,
         chaseAvatar,
         tigerAvatar,
         strifeAvatar,
         angelAvatar,
-        reinheitAvatar,
+        winstonAvatar,
         tropicalAvatar,
-        karnageAvatar,
         zarelAvatar,
-        forestAvatar
+        banditAvatar,
+        astroAvatar
     ] = await Promise.all([
         // IDs are in order like above
         "265569046425108481",
-        "219117197178568708",
         "396459305961914369",
         "291607550825332736",
         "109710323094683648",
         "176371068448145408",
+        "219117197178568708",
         "422650822254526474",
+        "183555431937998849",
         "292452611200909313",
         "267103945346908161",
         "469753186962374657",
         "280864874878337024",
         "286942968550785025",
         "547950545910890498",
-        "183555431937998849",
         "538148951614816256",
-        "469408379878506496"
+        "487003860041203712",
+        "299602021475155969"
     ].map((id) => users.fetch(id, true)
          .then((user) => user.displayAvatarURL({ format: user.avatar.startsWith("a_") ? "gif" : "png", size: 2048 }))
          .catch(() => "https://cdn.discordapp.com/embed/avatars/0.png")
@@ -48,21 +50,22 @@ router.get("/", async (req, res) => {
     res.render("index.ejs", {
         avatars: {
             benAvatar,
-            austinAvatar,
             toastAvatar,
             jackAvatar,
             kelwingAvatar,
             lewisAvatar,
+            austinAvatar,
             jaspAvatar,
+            karnageAvatar,
             chaseAvatar,
             tigerAvatar,
             strifeAvatar,
             angelAvatar,
-            reinheitAvatar,
+            winstonAvatar,
             tropicalAvatar,
-            karnageAvatar,
             zarelAvatar,
-            forestAvatar
+            banditAvatar,
+            astroAvatar
         },
         guilds: {
             furRetreat
